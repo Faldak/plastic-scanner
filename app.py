24f3dfd00dda3a91746c5780c29b12abae6dfd7b
Faldak <jetikezen@gmail.com>
2026-04-13 14:16:43 +0500

    Update app.py
    апявпр

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ ROBOFLOW_API_KEY = os.environ.get("ROBOFLOW_API_KEY", "GPKmLKlpIhdozMQ7STZs")
 # https://universe.roboflow.com/swu-deep-learning-project/plastic-recycling-code
 ROBOFLOW_MODEL_ID = os.environ.get(
     "ROBOFLOW_MODEL_ID",
-    " trash-3ydki/2"   # замените на ID вашей модели из Universe
+    " garbage-classification-3/2"   # замените на ID вашей модели из Universe
 )
 
 # База знаний по всем типам пластика
